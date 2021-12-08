d2e0bdcfe6dfc349cd6a976e02b4cfecf873ec63
ldl <2510952840@qq.com>
2021-12-08 18:56:23 +0800

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/current': { page: '/current' },
-      '/archive': { page: '/archive' },
-    }
-  },
 }
